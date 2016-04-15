ded27e5aa9ae5b4754a94d99746e5b804f135011
Alex Gibbons <alex.gibbons@gmail.com>
2016-04-15 00:18:14 -0700

    Add example usage to dummy app

---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  completedTodos: Ember.computed.filterBy('model', 'isComplete'),
+
+  onToggle(todoId) {
+    let todos = this.get('model').map((todo) => {
+      if (todo.id === todoId) {
+        todo.isComplete = !todo.isComplete;
+      }
+
+      return todo;
+    });
+
+    this.set('model', todos);
+  }
+});
