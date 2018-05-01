008d0e78f6a7639b5cab054213b3180c970c3dee
Nathan Zender <1099906+zendern@users.noreply.github.com>
2018-05-01 12:46:48 -0400

    Update to ember:try versions.
    Due to the version of ember-cli and thus ember:try using version
    compatibility only would go up to the 2.18 release line and then run
    beta/canary version for the 3.0 line.
    
    Now that ember 3 is a release candidate we want to actually run the
    tests against that release version too.
    
    So for now until the project is upgraded to a newer version of ember
    going to just hard code some versions.
    
    Using the ember docs went with the last few LTS releases (2.10,2.12,2.16 and 2.18) and then both
    minor versions of the 3.x (3.0 and 3.1) train.

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,4 +1,54 @@
-/*jshint node:true*/
+/* eslint-env node */
 module.exports = {
-  useVersionCompatibility: true
+  scenarios: [
+    {
+      name: 'ember-2.10',
+      bower: {
+        dependencies: {
+          ember: '~2.10.0'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.12.0'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.16',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.16.0'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.18',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.18.0'
+        }
+      }
+    },
+    {
+      name: 'ember-3.0.x',
+      npm: {
+        devDependencies: {
+          'ember-source': '~3.0.0'
+        }
+      }
+    },
+    {
+
+      name: 'ember-3.1.x',
+      npm: {
+        devDependencies: {
+          'ember-source': '~3.1.0'
+        }
+      }
+    }
+  ]
 };
