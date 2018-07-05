d935134c5d3ad8726287cddfb7df9fba8bb0ddf2
PSWai <parksoon_my@hotmail.com>
2018-07-05 10:20:56 +0800

    Add ember-auto-import to blueprint

--- blueprints/ember-cli-react/index.js
+++ blueprints/ember-cli-react/index.js
@@ -1,5 +1,24 @@
 /*jshint node:true*/
 
+var pkg = require('../../package.json');
+
+function getDependencyVersion(packageJson, name) {
+  var dependencies = packageJson.dependencies;
+  var devDependencies = packageJson.devDependencies;
+
+  return dependencies[name] || devDependencies[name];
+}
+
 module.exports = {
   description: 'Install ember-cli-react dependencies into your app.',
+
+  normalizeEntityName: function() {},
+
+  // Install react into host app
+  afterInstall: function() {
+    return this.addPackageToProject(
+      'ember-auto-import',
+      getDependencyVersion(pkg, 'ember-auto-import')
+    );
+  },
 };
