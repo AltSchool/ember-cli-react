6efb409b1d7be2fe7fd87d61219324e27826f783
PSWai <parksoon_my@hotmail.com>
2018-07-05 08:37:47 +0800

    Make react and react-dom peerDependencies

--- blueprints/ember-cli-react/index.js
+++ blueprints/ember-cli-react/index.js
@@ -1,32 +1,5 @@
 /*jshint node:true*/
 
-var RSVP = require('rsvp');
-var pkg = require('../../package.json');
-
-function getDependencyVersion(packageJson, name) {
-  var dependencies = packageJson.dependencies;
-  var devDependencies = packageJson.devDependencies;
-
-  return dependencies[name] || devDependencies[name];
-}
-
 module.exports = {
   description: 'Install ember-cli-react dependencies into your app.',
-
-  normalizeEntityName: function() {},
-
-  // Install react into host app
-  afterInstall: function() {
-    return RSVP.all([
-      this.addPackageToProject(
-        'ember-browserify',
-        getDependencyVersion(pkg, 'ember-browserify')
-      ),
-      this.addPackageToProject('react', getDependencyVersion(pkg, 'react')),
-      this.addPackageToProject(
-        'react-dom',
-        getDependencyVersion(pkg, 'react-dom')
-      ),
-    ]);
-  },
 };
