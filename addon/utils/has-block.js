968138a622cd972c0f4c347b01abf21b6d4cd65a
PSWai <parksoon_my@hotmail.com>
2018-07-06 16:16:14 +0800

    Add hasBlock util for checking if a component is in block form

---
+++ addon/utils/has-block.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+
+const [major, minor] = Ember.VERSION.split('.');
+const isGlimmer = major > 2 || (major == 2 && minor >= 10); // >= 2.10
+
+let hasBlockSymbol;
+
+if (major == 3 && minor >= 1) {
+  // do nothing since the symbol is not exported
+} else if (isGlimmer) {
+  hasBlockSymbol = Ember.__loader.require('ember-glimmer/component')[
+    'HAS_BLOCK'
+  ];
+} else {
+  hasBlockSymbol = Ember.__loader.require('ember-htmlbars/component')[
+    'HAS_BLOCK'
+  ];
+}
+
+// NOTE: I really don't know how to test this
+export default function hasBlock(emberComponent) {
+  // Since Glimmer moved to TypeScript, we can't get the symbol.
+  // This is a terrible but working way to get the value.
+  if (!hasBlockSymbol) {
+    const regex = /HAS_BLOCK/;
+    hasBlockSymbol = Object.keys(emberComponent).find(key => regex.test(key));
+  }
+
+  return Ember.get(emberComponent, hasBlockSymbol);
+}
